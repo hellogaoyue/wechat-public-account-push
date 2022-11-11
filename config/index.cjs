603e2a54eb204a9ec6262ec2ef0cce75efb5f55a
hellogaoyue <100398262+hellogaoyue@users.noreply.github.com>
2022-11-11 22:30:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oidgy6UuWSAEaNO7FR1LLjGwBVhQ',
+      id: 'oidgy6Y6gi2GMhdVx2UtII0cihWI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '7YoN9lztj0VZDVm33KXelCyoUzaZ3J8VY0hnS1cOgvc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
